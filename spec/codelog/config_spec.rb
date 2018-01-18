8ebf6085190ff3cad7d7997b37b5457ab524cfcb
Renan Hidemi Kataoka <renan.kataoka@codus.com.br>
2018-01-18 09:06:38 -0200

    Safe config files

---
+++ spec/codelog/config_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# describe Codelog::Config do
+#   before :each do
+#     allow(YAML).to receive(:load_file).with('changelogs/codelog.yml') do
+#       {
+#         'header_text' => 'my_header_text',
+#         'default_filename' => 'my_default_filename',
+#         'version_prefix' => "[v"
+#     version_suffix: "]"
+# date_prefix: ""
+# date_suffix: ""'
+#       }
+#   end
+# end
